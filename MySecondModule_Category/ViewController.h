35b75f68fbdf9ec1bf51f0abb9ccdd287099d451
黄成 <387241256@qq.com>
2019-01-25 15:20:44 +0800

    Initial Commit

---
+++ MySecondModule_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySecondModule_Category
+//
+//  Created by 黄成 on 2019/1/25.
+//  Copyright © 2019 gjzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MySecondModule_Category/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MySecondModule_Category
+//
+//  Created by 黄成 on 2019/1/25.
+//  Copyright © 2019 gjzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
